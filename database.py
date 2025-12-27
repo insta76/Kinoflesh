5d018c5ee45d9419b4808d0f0a3d6df6afbc8338
insta76 <instasotil@gmail.com>
2025-12-27 20:27:53 +0500

    database.py

--- database.py
+++ database.py
@@ -1,20 +1,17 @@
 from pymongo import MongoClient
 import os
 
-MONGO_URI = "mongodb+srv://ytshohrux1_db_user:s2fFP6UE7QFpDr3E@cluster0.eo1vapi.mongodb.net/?appName=Cluster0"
+MONGO_URI = os.getenv("MONGO_URI")
 client = MongoClient(MONGO_URI)
 db = client["kino_bot"]
 
-# Kolleksiyalar
 users_col = db["users"]
 pending_videos_col = db["pending_videos"]
 approved_videos_col = db["approved_videos"]
 channels_col = db["channels"]
 admins_col = db["admins"]
 
-# Asosiy admin
 MAIN_ADMIN_ID = 7162630033
 
-# Dastlabki adminni qo'shish
 if not admins_col.find_one({"user_id": MAIN_ADMIN_ID}):
     admins_col.insert_one({"user_id": MAIN_ADMIN_ID})
